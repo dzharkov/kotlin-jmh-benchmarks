98ccd688580219f220b196617a61af4c2782681e
Denis Zharkov <bintree@eblans.org>
2014-04-07 21:01:43 +0400

    Cleaned switch test

--- kotlin/src/main/kotlin/SwitchTest.kt
+++ kotlin/src/main/kotlin/SwitchTest.kt
@@ -13,177 +13,165 @@ import java.util.concurrent.TimeUnit
 [BenchmarkMode(Mode.AverageTime) ]
 [OutputTimeUnit(TimeUnit.NANOSECONDS)]
 public open class SwitchTestKotlin() {
-    open fun g(u : Int) : Int {
-        if (u > 100)
-        {
-            return 0
-        }
-
-        return u + 10
-    }
-
-    open fun f1(u : Int) : Int {
-        var t = 2
+    open fun sparseSwitch(u : Int) : Int {
+        var t : Int
         when (u) {
-            (1) -> {
+            1 -> {
                 t = 1
             }
-            (100500) -> {
+            100500 -> {
                 t = 2
             }
-            (2) -> {
+            2 -> {
                 t = 3
             }
-            (3) -> {
+            3 -> {
                 t = 4
             }
-            (4) -> {
+            4 -> {
                 t = 5
             }
-            (5) -> {
+            5 -> {
                 t = 6
             }
-            (6) -> {
+            6 -> {
                 t = 7
             }
-            (7) -> {
+            7 -> {
                 t = 1
             }
-            (8) -> {
+            8 -> {
                 t = 9
             }
-            (9) -> {
+            9 -> {
                 t = 1
             }
-            (10) -> {
+            10 -> {
                 t = 2
             }
-            (11) -> {
+            11 -> {
                 t = 3
             }
-            (12) -> {
+            12 -> {
                 t = 4
             }
-            (13) -> {
+            13 -> {
                 t = 4
             }
-            (14) -> {
+            14 -> {
                 t = 4
             }
-            (15) -> {
+            15 -> {
                 t = 435
             }
-            (16) -> {
+            16 -> {
                 t = 31
             }
-            (17) -> {
+            17 -> {
                 t = 1
             }
-            (18) -> {
+            18 -> {
                 t = 1
             }
-            (19) -> {
+            19 -> {
                 t = 1
             }
-            (20) -> {
+            20 -> {
                 t = 1
             }
             else -> {
                 t = 5
             }
         }
-        return g(t)
-    }
-    [GenerateMicroBenchmark]
-    public open fun testSwitch1() : Int {
-        var sum = 0
-        val n = ITERATIONS
-        for (i in 0..n - 1) {
-            sum += f1(i)
-        }
-        return sum
+        return t
     }
 
-    open fun f2(u : Int) : Int {
-        var t = 2
+    [GenerateMicroBenchmark]
+    public open fun testSparseSwitch(bh : BlackHole) {
+        val n = ITERATIONS
+        for (i in 0..n - 1) {
+            bh.consume(sparseSwitch(i))
+        }
+    }
+
+    open fun denseSwitch(u : Int) : Int {
+        var t : Int
         when (u) {
-            (1) -> {
+            1 -> {
                 t = 1
             }
-            (-1) -> {
+            -1 -> {
                 t = 2
             }
-            (2) -> {
+            2 -> {
                 t = 3
             }
-            (3) -> {
+            3 -> {
                 t = 4
             }
-            (4) -> {
+            4 -> {
                 t = 5
             }
-            (5) -> {
+            5 -> {
                 t = 6
             }
-            (6) -> {
+            6 -> {
                 t = 7
             }
-            (7) -> {
+            7 -> {
                 t = 1
             }
-            (8) -> {
+            8 -> {
                 t = 9
             }
-            (9) -> {
+            9 -> {
                 t = 1
             }
-            (10) -> {
+            10 -> {
                 t = 2
             }
-            (11) -> {
+            11 -> {
                 t = 3
             }
-            (12) -> {
+            12 -> {
                 t = 4
             }
-            (13) -> {
+            13 -> {
                 t = 4
             }
-            (14) -> {
+            14 -> {
                 t = 4
             }
-            (15) -> {
+            15 -> {
                 t = 435
             }
-            (16) -> {
+            16 -> {
                 t = 31
             }
-            (17) -> {
+            17 -> {
                 t = 1
             }
-            (18) -> {
+            18 -> {
                 t = 1
             }
-            (19) -> {
+            19 -> {
                 t = 1
             }
-            (20) -> {
+            20 -> {
                 t = 1
             }
             else -> {
                 t = 5
             }
         }
-        return g(t)
+        return t
     }
     [GenerateMicroBenchmark]
-    public open fun testSwitch2() : Int {
-        var sum = 0
+    public open fun testDenseSwitch(bh : BlackHole) {
         val n = ITERATIONS
         for (i in 0..n - 1) {
-            sum += f2(i)
+            bh.consume(denseSwitch(i))
         }
-        return sum
     }
 
     class object {
