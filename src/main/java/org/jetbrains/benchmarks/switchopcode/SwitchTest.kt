9928dfe124375a69060d9bf351f90de887217008
Denis Zharkov <denis.zharkov@jetbrains.com>
2015-02-26 20:48:26 +0300

    Add benchmarks for when by enums and strings

--- src/main/java/org/jetbrains/benchmarks/switchopcode/SwitchTest.kt
+++ src/main/java/org/jetbrains/benchmarks/switchopcode/SwitchTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.benchmarks
+package org.jetbrains.benchmarks.switchopcode
 
 import org.openjdk.jmh.annotations.*;
 
@@ -172,5 +172,154 @@ public open class SwitchTestKotlin() {
         }
     }
 
+    var enums: Array<MyEnum> = array()
+
+    Setup
+    public fun initEnums() {
+        enums = Array(iterations) {
+            i -> MyEnum.values()[i % MyEnum.values().size]
+        }
+    }
+
+    [CompilerControl(CompilerControl.Mode.DONT_INLINE)]
+    private fun enumSwitch(x: MyEnum): Int {
+        when (x) {
+            MyEnum.ITEM2 -> return 1
+            MyEnum.ITEM4 -> return 2
+            MyEnum.ITEM6 -> return 3
+            MyEnum.ITEM8 -> return 4
+            MyEnum.ITEM10 -> return 5
+            MyEnum.ITEM12 -> return 6
+            MyEnum.ITEM14 -> return 7
+            MyEnum.ITEM16 -> return 8
+            MyEnum.ITEM18 -> return 9
+            MyEnum.ITEM20 -> return 10
+            MyEnum.ITEM22 -> return 11
+            MyEnum.ITEM24 -> return 12
+            MyEnum.ITEM26 -> return 13
+            MyEnum.ITEM28 -> return 14
+            MyEnum.ITEM30 -> return 15
+            MyEnum.ITEM32 -> return 16
+            MyEnum.ITEM34 -> return 17
+            MyEnum.ITEM36 -> return 18
+            MyEnum.ITEM38 -> return 19
+            MyEnum.ITEM40 -> return 20
+            MyEnum.ITEM42 -> return 21
+            MyEnum.ITEM44 -> return 22
+            MyEnum.ITEM46 -> return 23
+            MyEnum.ITEM48 -> return 24
+            MyEnum.ITEM50 -> return 25
+            MyEnum.ITEM52 -> return 26
+            MyEnum.ITEM54 -> return 27
+            MyEnum.ITEM56 -> return 28
+            MyEnum.ITEM58 -> return 29
+            MyEnum.ITEM60 -> return 30
+            MyEnum.ITEM62 -> return 31
+            MyEnum.ITEM64 -> return 32
+            MyEnum.ITEM66 -> return 33
+            MyEnum.ITEM68 -> return 34
+            MyEnum.ITEM70 -> return 35
+            MyEnum.ITEM72 -> return 36
+            MyEnum.ITEM74 -> return 37
+            MyEnum.ITEM76 -> return 38
+            MyEnum.ITEM78 -> return 39
+            MyEnum.ITEM80 -> return 40
+            MyEnum.ITEM82 -> return 41
+            MyEnum.ITEM84 -> return 42
+            MyEnum.ITEM86 -> return 43
+            MyEnum.ITEM88 -> return 44
+            MyEnum.ITEM90 -> return 45
+            MyEnum.ITEM92 -> return 46
+            MyEnum.ITEM94 -> return 47
+            MyEnum.ITEM96 -> return 48
+            MyEnum.ITEM98 -> return 49
+            MyEnum.ITEM100 -> return 50
+            else -> return -1
+        }
+    }
+
+    Benchmark
+    public fun testEnumSwitch(bh: Blackhole) {
+        val localEnums = enums
+        val n = iterations
+        for (i in 0..n - 1) {
+            bh.consume(enumSwitch(localEnums[i]))
+        }
+    }
+
+    var strings: Array<String> = array()
+
+    Setup
+    public fun initStrings() {
+        strings = Array(iterations) { i ->
+            "ABCDEFGHIJKLMNO${((i % 50) + 1)}"
+        }
+    }
+
+    [CompilerControl(CompilerControl.Mode.DONT_INLINE)]
+    private fun stringSwitch(s: String): Int {
+        when (s) {
+            "ABCDEFGHIJKLMNO1" -> return 1
+            "ABCDEFGHIJKLMNO2" -> return 2
+            "ABCDEFGHIJKLMNO3" -> return 3
+            "ABCDEFGHIJKLMNO4" -> return 4
+            "ABCDEFGHIJKLMNO5" -> return 5
+            "ABCDEFGHIJKLMNO6" -> return 6
+            "ABCDEFGHIJKLMNO7" -> return 7
+            "ABCDEFGHIJKLMNO8" -> return 8
+            "ABCDEFGHIJKLMNO9" -> return 9
+            "ABCDEFGHIJKLMNO10" -> return 10
+            "ABCDEFGHIJKLMNO11" -> return 11
+            "ABCDEFGHIJKLMNO12" -> return 12
+            "ABCDEFGHIJKLMNO13" -> return 13
+            "ABCDEFGHIJKLMNO14" -> return 14
+            "ABCDEFGHIJKLMNO15" -> return 15
+            "ABCDEFGHIJKLMNO16" -> return 16
+            "ABCDEFGHIJKLMNO17" -> return 17
+            "ABCDEFGHIJKLMNO18" -> return 18
+            "ABCDEFGHIJKLMNO19" -> return 19
+            "ABCDEFGHIJKLMNO20" -> return 20
+            "ABCDEFGHIJKLMNO21" -> return 21
+            "ABCDEFGHIJKLMNO22" -> return 22
+            "ABCDEFGHIJKLMNO23" -> return 23
+            "ABCDEFGHIJKLMNO24" -> return 24
+            "ABCDEFGHIJKLMNO25" -> return 25
+            "ABCDEFGHIJKLMNO26" -> return 26
+            "ABCDEFGHIJKLMNO27" -> return 27
+            "ABCDEFGHIJKLMNO28" -> return 28
+            "ABCDEFGHIJKLMNO29" -> return 29
+            "ABCDEFGHIJKLMNO30" -> return 30
+            "ABCDEFGHIJKLMNO31" -> return 31
+            "ABCDEFGHIJKLMNO32" -> return 32
+            "ABCDEFGHIJKLMNO33" -> return 33
+            "ABCDEFGHIJKLMNO34" -> return 34
+            "ABCDEFGHIJKLMNO35" -> return 35
+            "ABCDEFGHIJKLMNO36" -> return 36
+            "ABCDEFGHIJKLMNO37" -> return 37
+            "ABCDEFGHIJKLMNO38" -> return 38
+            "ABCDEFGHIJKLMNO39" -> return 39
+            "ABCDEFGHIJKLMNO40" -> return 40
+            "ABCDEFGHIJKLMNO41" -> return 41
+            "ABCDEFGHIJKLMNO42" -> return 42
+            "ABCDEFGHIJKLMNO43" -> return 43
+            "ABCDEFGHIJKLMNO44" -> return 44
+            "ABCDEFGHIJKLMNO45" -> return 45
+            "ABCDEFGHIJKLMNO46" -> return 46
+            "ABCDEFGHIJKLMNO47" -> return 47
+            "ABCDEFGHIJKLMNO48" -> return 48
+            "ABCDEFGHIJKLMNO49" -> return 49
+            "ABCDEFGHIJKLMNO50" -> return 50
+            else -> return -1
+        }
+    }
+
+    Benchmark
+    public fun testStringSwitch(bh: Blackhole) {
+        val localStrings = strings
+        val n = iterations
+        for (i in 0..n - 1) {
+            bh.consume(stringSwitch(localStrings[i]))
+        }
+    }
 }
 
--- src/main/java/org/jetbrains/benchmarks/switchopcode/SwitchTestJava.java
+++ src/main/java/org/jetbrains/benchmarks/switchopcode/SwitchTestJava.java
@@ -127,4 +127,157 @@ public class SwitchTestJava {
             bh.consume(denseSwitch(i));
         }
     }
+
+    MyEnum[] enums;
+
+    @Setup
+    public void initEnums() {
+        enums = new MyEnum[iterations];
+        for (int i = 0; i < iterations; i++) {
+            enums[i] = MyEnum.values()[i % MyEnum.values().length];
+        }
+    }
+
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    private int enumSwitch(MyEnum x) {
+        switch (x) {
+            case ITEM2: return 1;
+            case ITEM4: return 2;
+            case ITEM6: return 3;
+            case ITEM8: return 4;
+            case ITEM10: return 5;
+            case ITEM12: return 6;
+            case ITEM14: return 7;
+            case ITEM16: return 8;
+            case ITEM18: return 9;
+            case ITEM20: return 10;
+            case ITEM22: return 11;
+            case ITEM24: return 12;
+            case ITEM26: return 13;
+            case ITEM28: return 14;
+            case ITEM30: return 15;
+            case ITEM32: return 16;
+            case ITEM34: return 17;
+            case ITEM36: return 18;
+            case ITEM38: return 19;
+            case ITEM40: return 20;
+            case ITEM42: return 21;
+            case ITEM44: return 22;
+            case ITEM46: return 23;
+            case ITEM48: return 24;
+            case ITEM50: return 25;
+            case ITEM52: return 26;
+            case ITEM54: return 27;
+            case ITEM56: return 28;
+            case ITEM58: return 29;
+            case ITEM60: return 30;
+            case ITEM62: return 31;
+            case ITEM64: return 32;
+            case ITEM66: return 33;
+            case ITEM68: return 34;
+            case ITEM70: return 35;
+            case ITEM72: return 36;
+            case ITEM74: return 37;
+            case ITEM76: return 38;
+            case ITEM78: return 39;
+            case ITEM80: return 40;
+            case ITEM82: return 41;
+            case ITEM84: return 42;
+            case ITEM86: return 43;
+            case ITEM88: return 44;
+            case ITEM90: return 45;
+            case ITEM92: return 46;
+            case ITEM94: return 47;
+            case ITEM96: return 48;
+            case ITEM98: return 49;
+            case ITEM100: return 50;
+            default:
+                return -1;
+        }
+    }
+
+    @Benchmark
+    public void testEnumSwitch(Blackhole bh) {
+        MyEnum[] localEnums = enums;
+        int n = iterations;
+        for (int i = 0; i < n; i++) {
+            bh.consume(enumSwitch(localEnums[i]));
+        }
+    }
+
+    String[] strings;
+
+    @Setup
+    public void initStrings() {
+        strings = new String[iterations];
+        for (int i = 0; i < iterations; i++) {
+            strings[i] = "ABCDEFGHIJKLMNO" + ((i % 50) + 1);
+        }
+    }
+
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    private int stringSwitch(String s) {
+        switch(s) {
+            case "ABCDEFGHIJKLMNO1": return 1;
+            case "ABCDEFGHIJKLMNO2": return 2;
+            case "ABCDEFGHIJKLMNO3": return 3;
+            case "ABCDEFGHIJKLMNO4": return 4;
+            case "ABCDEFGHIJKLMNO5": return 5;
+            case "ABCDEFGHIJKLMNO6": return 6;
+            case "ABCDEFGHIJKLMNO7": return 7;
+            case "ABCDEFGHIJKLMNO8": return 8;
+            case "ABCDEFGHIJKLMNO9": return 9;
+            case "ABCDEFGHIJKLMNO10": return 10;
+            case "ABCDEFGHIJKLMNO11": return 11;
+            case "ABCDEFGHIJKLMNO12": return 12;
+            case "ABCDEFGHIJKLMNO13": return 13;
+            case "ABCDEFGHIJKLMNO14": return 14;
+            case "ABCDEFGHIJKLMNO15": return 15;
+            case "ABCDEFGHIJKLMNO16": return 16;
+            case "ABCDEFGHIJKLMNO17": return 17;
+            case "ABCDEFGHIJKLMNO18": return 18;
+            case "ABCDEFGHIJKLMNO19": return 19;
+            case "ABCDEFGHIJKLMNO20": return 20;
+            case "ABCDEFGHIJKLMNO21": return 21;
+            case "ABCDEFGHIJKLMNO22": return 22;
+            case "ABCDEFGHIJKLMNO23": return 23;
+            case "ABCDEFGHIJKLMNO24": return 24;
+            case "ABCDEFGHIJKLMNO25": return 25;
+            case "ABCDEFGHIJKLMNO26": return 26;
+            case "ABCDEFGHIJKLMNO27": return 27;
+            case "ABCDEFGHIJKLMNO28": return 28;
+            case "ABCDEFGHIJKLMNO29": return 29;
+            case "ABCDEFGHIJKLMNO30": return 30;
+            case "ABCDEFGHIJKLMNO31": return 31;
+            case "ABCDEFGHIJKLMNO32": return 32;
+            case "ABCDEFGHIJKLMNO33": return 33;
+            case "ABCDEFGHIJKLMNO34": return 34;
+            case "ABCDEFGHIJKLMNO35": return 35;
+            case "ABCDEFGHIJKLMNO36": return 36;
+            case "ABCDEFGHIJKLMNO37": return 37;
+            case "ABCDEFGHIJKLMNO38": return 38;
+            case "ABCDEFGHIJKLMNO39": return 39;
+            case "ABCDEFGHIJKLMNO40": return 40;
+            case "ABCDEFGHIJKLMNO41": return 41;
+            case "ABCDEFGHIJKLMNO42": return 42;
+            case "ABCDEFGHIJKLMNO43": return 43;
+            case "ABCDEFGHIJKLMNO44": return 44;
+            case "ABCDEFGHIJKLMNO45": return 45;
+            case "ABCDEFGHIJKLMNO46": return 46;
+            case "ABCDEFGHIJKLMNO47": return 47;
+            case "ABCDEFGHIJKLMNO48": return 48;
+            case "ABCDEFGHIJKLMNO49": return 49;
+            case "ABCDEFGHIJKLMNO50": return 50;
+            default: return -1;
+        }
+    }
+
+    @Benchmark
+    public void testStringSwitch(Blackhole bh) {
+        String localStrings[] = strings;
+        int n = iterations;
+        for (int i = 0; i < n; i++) {
+            bh.consume(stringSwitch(localStrings[i]));
+        }
+    }
 }
