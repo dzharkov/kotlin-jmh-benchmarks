009611c7a2aa899b785241bf3b0477367904d44d
Denis Zharkov <denis.zharkov@jetbrains.com>
2015-04-19 19:09:25 +0300

    Stars reformat

--- src/main/java/org/jetbrains/benchmarks/stars/Stars.kt
+++ src/main/java/org/jetbrains/benchmarks/stars/Stars.kt
@@ -1,128 +1,119 @@
 package org.jetbrains.benchmarks.stars
 
-import org.openjdk.jmh.annotations.*;
-
+import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.infra.Blackhole
 import java.util.Comparator
 import java.util.concurrent.TimeUnit
-import org.openjdk.jmh.infra.*
 
 [State(Scope.Thread)]
-[BenchmarkMode(Mode.AverageTime) ]
+[BenchmarkMode(Mode.AverageTime)]
 [OutputTimeUnit(TimeUnit.NANOSECONDS)]
 open public class StarsKotlin() {
     public open class ComparableComparator<T : Comparable<T>>() : Comparator<T?> {
-        public override fun compare(lhs : T?, rhs : T?) : Int {
+        public override fun compare(lhs: T?, rhs: T?): Int {
             return lhs!!.compareTo(rhs!!)
         }
     }
 
-    open class AVLTree<K, D>(var comparator : Comparator<K?>) {
+    open class AVLTree<K, D>(var comparator: Comparator<K?>) {
 
         class NodeGetter {
-            fun height<K, D>(node : Node<K, D>?) : Int {
+            fun height<K, D>(node: Node<K, D>?): Int {
                 return node?.height ?: 0
             }
 
-            fun size<K, D>(node : Node<K, D>?) : Int {
+            fun size<K, D>(node: Node<K, D>?): Int {
                 return node?.size ?: 0
             }
         }
 
         class object {
-            val ng : NodeGetter = NodeGetter()
+            val ng: NodeGetter = NodeGetter()
         }
 
 
-        inner class Node<K,D>(var key : K, var data : D) {
+        inner class Node<K, D>(var key: K, var data: D) {
 
-            var size : Int = 1
-            var left : Node<K,D>? = null
+            var size: Int = 1
+            var left: Node<K, D>? = null
                 set(value) {
-                    if (!value.identityEquals($left))
-                    {
+                    if (!value.identityEquals($left)) {
                         $left = value
                         if ($left != null) {
                             $left!!.parent = this
                         }
                     }
                 }
-            var right : Node<K,D>? = null
+            var right: Node<K, D>? = null
                 set(value) {
-                    if (!value.identityEquals($right))
-                    {
+                    if (!value.identityEquals($right)) {
                         $right = value
                         if ($right != null) {
                             $right!!.parent = this
                         }
                     }
                 }
-            var parent : Node<K,D>? = null
-            var height : Int = 1
-            var balance : Int = 0
-            fun updateHeight() : Unit {
-                val leftHeight = ng.height<K,D>(left)
+            var parent: Node<K, D>? = null
+            var height: Int = 1
+            var balance: Int = 0
+            fun updateHeight(): Unit {
+                val leftHeight = ng.height<K, D>(left)
                 val rightHeight = ng.height(right)
                 height = 1 + Math.max(leftHeight, rightHeight)
             }
-            fun updateBalance() : Unit {
+
+            fun updateBalance(): Unit {
                 left?.updateHeight()
                 right?.updateHeight()
-                val leftHeight = ng.height<K,D>(left)
+                val leftHeight = ng.height<K, D>(left)
                 val rightHeight = ng.height(right)
                 balance = leftHeight - rightHeight
             }
 
-            fun onChildrenChange() : Unit {
+            fun onChildrenChange(): Unit {
                 updateBalance()
                 updateSize()
             }
 
-            fun updateSize() : Unit {
+            fun updateSize(): Unit {
                 val leftSize = ng.size(left)
                 val rightSize = ng.size(right)
-                size = 1 + leftSize +  rightSize
+                size = 1 + leftSize + rightSize
             }
         }
-        protected open fun buildNode<K,D>(key : K, data : D) : Node<K,D> {
-            return Node<K,D>(key, data)
+
+        protected open fun buildNode<K, D>(key: K, data: D): Node<K, D> {
+            return Node<K, D>(key, data)
         }
-        var root : Node<K,D>? = null
-        fun insert(key : K, data : D) : Unit {
+
+        var root: Node<K, D>? = null
+        fun insert(key: K, data: D): Unit {
             root = insert(root, key, data)
             root!!.parent = null
             root!!.onChildrenChange()
         }
-        fun insert(node : Node<K,D>?, key : K, data : D) : Node<K,D> {
-            if (node == null)
-            {
-                return buildNode<K,D>(key, data)
+
+        fun insert(node: Node<K, D>?, key: K, data: D): Node<K, D> {
+            if (node == null) {
+                return buildNode<K, D>(key, data)
             }
 
-            if (comparator.compare(node.key, key) > 0)
-            {
+            if (comparator.compare(node.key, key) > 0) {
                 node.left = (insert(node.left, key, data))
-            }
-            else
-            {
+            } else {
                 node.right = (insert(node.right, key, data))
             }
 
             node.onChildrenChange()
-            if (Math.abs(node.balance) > 1)
-            {
-                if (node.balance > 1)
-                {
-                    if (node.left!!.balance < 0)
-                    {
+            if (Math.abs(node.balance) > 1) {
+                if (node.balance > 1) {
+                    if (node.left!!.balance < 0) {
                         node.left = (rotateLeft(node.left!!))
                     }
 
                     return rotateRight(node)
-                }
-                else
-                {
-                    if (node.right!!.balance > 0)
-                    {
+                } else {
+                    if (node.right!!.balance > 0) {
                         node.right = (rotateRight(node.right!!))
                     }
 
@@ -132,7 +123,8 @@ open public class StarsKotlin() {
 
             return node
         }
-        private fun rotateRight(node : Node<K,D>) : Node<K,D> {
+
+        private fun rotateRight(node: Node<K, D>): Node<K, D> {
             val wasLeft = node.left!!
             node.left = (wasLeft.right)
             node.onChildrenChange()
@@ -140,7 +132,8 @@ open public class StarsKotlin() {
             wasLeft.onChildrenChange()
             return wasLeft
         }
-        private fun rotateLeft(node : Node<K,D>) : Node<K,D> {
+
+        private fun rotateLeft(node: Node<K, D>): Node<K, D> {
             val wasRight = node.right!!
             node.right = (wasRight.left)
             node.onChildrenChange()
@@ -148,70 +141,60 @@ open public class StarsKotlin() {
             wasRight.onChildrenChange()
             return wasRight
         }
-        fun find(key : K) : Node<K,D>? {
+
+        fun find(key: K): Node<K, D>? {
             return find(root, key)
         }
 
-        fun find(node : Node<K,D>?, key : K) : Node<K,D>? {
-            if (node == null)
-            {
+        fun find(node: Node<K, D>?, key: K): Node<K, D>? {
+            if (node == null) {
                 return null
             }
 
-            if (node.key.equals(key))
-            {
+            if (node.key.equals(key)) {
                 return node
             }
 
-            if (comparator.compare(node.key, key) > 0)
-            {
+            if (comparator.compare(node.key, key) > 0) {
                 return find(node.left, key)
-            }
-            else
-            {
+            } else {
                 return find(node.right, key)
             }
         }
+
         {
             this.comparator = comparator
         }
 
-        fun elementOrder(key : K) : Int {
+        fun elementOrder(key: K): Int {
             var node = find(key)
-            if (node == null)
-            {
+            if (node == null) {
                 return -1
             }
 
             var result = 0
-            do
-            {
-                if (node!!.left != null)
-                {
-                    result += ((node!!.left as Node<K,D>)).size
+            do {
+                if (node!!.left != null) {
+                    result += ((node!!.left as Node<K, D>)).size
                 }
 
-                while (node!!.parent?.left == node)
-                {
+                while (node!!.parent?.left == node) {
                     node = node!!.parent
                 }
-                if (node!!.parent != null)
-                {
+                if (node!!.parent != null) {
                     result++
                 }
 
                 node = node!!.parent
-            }
-            while (node != null)
+            } while (node != null)
             return result
         }
 
     }
 
-    class PointY(var y : Int, var id : Int) : Comparable<PointY> {
-        public override fun compareTo(o : PointY) : Int {
-            if (y != o.y)
-            {
+    class PointY(var y: Int, var id: Int) : Comparable<PointY> {
+        public override fun compareTo(o: PointY): Int {
+            if (y != o.y) {
                 return y - o.y
             }
 
@@ -220,7 +203,7 @@ open public class StarsKotlin() {
     }
 
     [Benchmark]
-    public fun solve(bh : Blackhole) : Unit {
+    public fun solve(bh: Blackhole): Unit {
         val tree = AVLTree<PointY, Int>(ComparableComparator<PointY>())
         val n = StarsData.getN()
         val result = IntArray(n)
